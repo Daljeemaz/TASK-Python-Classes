581647611daf15b8647b9a9c6043ec7e8a5e92dc
Duaij <Daljeemaz@gmail.com>
2022-10-02 17:46:46 +0300

    complete the task

--- main.py
+++ main.py
@@ -1,38 +1,67 @@
 class Wallet:
     def __init__(self, money):
-        pass
+        self.money = money
 
-    def credit(amount):
-        pass
+    def credit(self, amount):
+        self.money += amount
 
-    def debit(amount):
-        pass
-
+    def debit(self, amount):
+        self.money -= amount
+    
 
 wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
+
 print(wallet)
 
 
+
 class Person:
-    # Implement the code here
-    pass
+
+    def __init__(self, name, location, money):
+        self.name = name
+        self.location = location
+        self.wallet = Wallet(money)
+
+    def moveTo(self, point):
+        self.location = point
+
+
 
 
 person = Person("Moh", 5, 50)
 
 
 class Vendor:
-    # implement Vendor!
-    pass
+    def __init__(self, name, location, money):
+        super().__init__(name, location, money)
+        self.range = 5
+        self.price = 1
+
+    def sellTo(self, customer, number_of_icecreams):
+        self.location = customer.location
+        self.wallet.credit(number_of_icecreams * self.price)
+        customer.wallet.debit(number_of_icecreams * self.price)
 
 
 vendor = Vendor("Abdallah", 3, 6)
 
 
 class Customer:
-    # implement Customer!
-    pass
+    def __init__(self, name, location, money):
+        super().__init__(name, location, money)
+
+    def _is_in_range(self, vendor):
+        distance = vendor.location - self.location
+        if distance > vendor.range:
+            return True
+        else:
+            return False
+
+    def _have_enough_money(self, vendor, number_of_icecreams):
+        if self.wallet.money >= vendor.price * number_of_icecreams:
+            return True
+        else:
+            return False
 
 
 customer = Customer("Abdallah", 3, 6)
